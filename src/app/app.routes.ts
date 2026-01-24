2f5818186ebf07e59492309af55bf463a4d274af
IslamCabarli <icabarli@std.beu.edu.az>
2026-01-25 02:37:00 +0400

    feat: add standalone routing for main application pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'cars',
+    loadComponent: () => import('./features/cars/cars.component').then(m => m.CarsComponent)
+  },
+  {
+    path: 'cars/:id',
+    loadComponent: () => import('./features/car-detail/car-detail.component').then(m => m.CarDetailComponent)
+  },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    loadComponent: () => import('./features/admin/admin.component').then(m => m.AdminComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
