06d558585f379dbdd45a07065109546c52270fe5
IslamCabarli <icabarli@std.beu.edu.az>
2026-01-26 20:06:17 +0400

    feat(layout): add header and footer to root layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  template: `
+    <app-header />
+    <router-outlet />
+    <app-footer />
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'mansory-frontend';
