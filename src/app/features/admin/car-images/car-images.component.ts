0b4e2d12f04d840d023541ade18454fb6ae5d8f9
IslamCabarli <icabarli@std.beu.edu.az>
2026-01-28 01:37:59 +0400

    feat(admin): add standalone CarImages component for car management

---
+++ src/app/features/admin/car-images/car-images.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-images',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-images.component.html',
+  styleUrl: './car-images.component.scss'
+})
+export class CarImagesComponent {
+
+}
