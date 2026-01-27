814b7606b6d49c06956c4fc030e7d6040b0d76e6
IslamCabarli <icabarli@std.beu.edu.az>
2026-01-28 00:47:57 +0400

    feat(app): integrate loading screen component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoadingScreenComponent } from './shared/components/loading-screen/loading-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoadingScreenComponent],
   template: `
+    <app-loading-screen />
     <app-header />
     <router-outlet />
     <app-footer />
