ff6bece1cc5c86ff56957c1ac5e9aad7f9b17b0d
IslamCabarli <icabarli@std.beu.edu.az>
2026-03-14 15:42:13 +0400

    feat(search): add car search component with brand and model filters

---
+++ src/app/features/model-finder/model-finder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-finder',
+  standalone: true,
+  imports: [],
+  templateUrl: './model-finder.component.html',
+  styleUrl: './model-finder.component.scss'
+})
+export class ModelFinderComponent {
+
+}
