f1268802c53a3ecad200847e6100fadc2330e186
IslamCabarli <icabarli@std.beu.edu.az>
2026-03-28 22:45:52 +0400

    refactor(header): change the configurator of car id

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent {
   isScrolled = signal(false);
 
   configuratorCars = [
-    { id: 1, name: 'CULLINAN II', detailUrl: '/cars/1' },
+    { id: 22, name: 'CULLINAN II', detailUrl: '/cars/1' },
     { id: 2, name: 'PUROSANGUE', detailUrl: '/cars/2' }
   ];
 
